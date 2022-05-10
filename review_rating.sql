b92c40a9facb7c059095fd887929798f35821ae7
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-11 00:01:28 +0530

    Update review_rating.sql

--- review_rating.sql
+++ review_rating.sql
@@ -1,8 +1,16 @@
 CREATE TABLE Review_Rating(review_rating_id INTEGER PRIMARY KEY, 
  						   review_score_rating INTEGER, 
-							 review_id INTEGER, 
+						   review_id INTEGER, 
 						   host_id INTEGER,
 						   FOREIGN KEY (review_id) REFERENCES reviews(review_id),
 						   FOREIGN KEY (host_id) REFERENCES hosts_entry(host_id));
                
                
+
+
+
+INSERT INTO Review_Rating VALUES(435, 7, 1, 23451 ); // 435added...
+// Make sure to match hosts_id and review_id
+----------YUVRAJ--------------
+----------SARTAAJ-------------
+----------ADITI---------------
