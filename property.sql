bb63d2473e981925fab0f2b8fd4171f6b656561a
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-11 00:08:59 +0530

    Create property.sql

---
+++ property.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Property(Property_id INTEGER PRIMARY KEY,
+					 Property_TYPE VARCHAR(50),
+					 Room_type VARCHAR(50),
+					 Accommodations INTEGER,
+					 Bathrooms INTEGER,
+					 Bedrooms INTEGER,
+					 Beds INTEGER,
+					 Bed_type VARCHAR(50),
+					 Sqaure_feet INT,
+					 Price INT,
+					 review_id INT,
+					 host_id INT,
+					 review_rating_id INTEGER,
+					  FOREIGN KEY (review_id) REFERENCES reviews(review_id),
+					  FOREIGN KEY (host_id) REFERENCES hosts_entry(host_id),
+					  FOREIGN KEY (review_rating_id) REFERENCES Review_rating(review_rating_id)
+					  )
