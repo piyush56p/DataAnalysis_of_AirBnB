9fc549858e352b31d0b36800a5b9e581f2c4bedc
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-11 21:20:47 +0530

    Update property.sql

--- property.sql
+++ property.sql
@@ -62,5 +62,31 @@ INSERT INTO Property VALUES(2,'House','Private room',1,1,1,1,'Real Bed',500,60,2
  INSERT INTO Property VALUES(25,'Condominium','Entire home/apt',4,1,1,2,'Real Bed',600,159,25,8796,459);
  INSERT INTO Property VALUES(26,'Condominium','Entire home/apt',2,1,1,1,'Real Bed',660,98,26,8123,460);
 -------------sartaaj--------------
+INSERT INTO Property VALUES(1,'Condominium','Entire home/apt',6,2,2,4,'Real Bed',1200,295,1,23451,435);
+INSERT INTO Property VALUES(27,'Condominium','Entire home/apt',2,1,0,1,'Real Bed',2500,1950,27,6758,461);
+INSERT INTO Property VALUES(28,'Condominium','Entire home/apt',4,1,1,1,'Real Bed',1000,98,28,1345,462);
+INSERT INTO Property VALUES(29,'House','Entire home/apt',3,2,2,2,'Real Bed',500,112,29,6729,463);
+INSERT INTO Property VALUES(30,'House','Entire home/apt',4,1.5,2,2,'Real Bed',1000,300,30,1425,464);
+INSERT INTO Property VALUES(31,'Condominium','Entire home/apt',4,1,2,2,'Real Bed',507,210,31,5621,465);
+INSERT INTO Property VALUES(32,'Guest suite','Private room',3,1,1,2,'Real Bed',684,98,32,1475,466);
+INSERT INTO Property VALUES(33,'Apartment','Entire home/apt',4,1,0,2,'Real Bed',542,56,33,8888,467);
+INSERT INTO Property VALUES(34,'Apartment','Entire home/apt',2,1,1,1,'Real Bed',576,119,34,6852,468);
+INSERT INTO Property VALUES(35,'Townhouse','Entire home/apt',3,1,1,1,'Real Bed',1264,105,35,6958,469);
+INSERT INTO Property VALUES(36,'Guest suite','Entire home/apt',3,1,2,2,'Real Bed',486,139,36,5471,470);
+INSERT INTO Property VALUES(37,'House','Private room',2,1,1,1,'Real Bed',458,60,37,1268,471);
+INSERT INTO Property VALUES(38,'Townhouse','Private room',2,1.5,1,1,'Real Bed',452,85,38,6666,472);
+INSERT INTO Property VALUES(39,'Apartment','Entire home/apt',3,1,1,2,'Real Bed',754,110,39,6423,473);
+INSERT INTO Property VALUES(40,'House','Private room',1,1,1,1,'Real Bed',468,45,40,7777,474);
+INSERT INTO Property VALUES(41,'Townhouse','Private room',2,1.5,1,1,'Real Bed',521,80,41,1753,475);
+INSERT INTO Property VALUES(42,'House','Private room',2,1,1,1,'Real Bed',785,39,42,5217,476);
+INSERT INTO Property VALUES(43,'Loft','Entire home/apt',2,1,1,2,'Real Bed',784,98,43,3497,477);
+INSERT INTO Property VALUES(44,'Condominium','Entire home/apt',3,1,1,1,'Real Bed',965,108,44,3841,478);
+INSERT INTO Property VALUES(45,'House','Entire home/apt',6,2,3,4,'Real Bed',987,175,45,9999,479);
+INSERT INTO Property VALUES(46,'Condominium','Entire home/apt',5,2,2,3,'Real Bed',999,251,46,8745,480);
+INSERT INTO Property VALUES(47,'Guest suite','Entire home/apt',2,1,1,2,'Real Bed',452,94,47,6513,481);
+INSERT INTO Property VALUES(48,'Loft','Private room',2,1,1,1,'Real Bed',856,94,48,9651,482);
+INSERT INTO Property VALUES(49,'Guest suite','Entire home/apt',4,1,1,2,'Real Bed',561,93,49,7541,483);
+INSERT INTO Property VALUES(50,'Condominium','Entire home/apt',5,2,2,2,'Real Bed',547,125,50,3154,484);
+INSERT INTO Property VALUES(51,'Townhouse','Entire home/apt',5,2.5,3,3,'Real Bed',660,209,51,9461,485);
 --------------aditi-------------------
 
