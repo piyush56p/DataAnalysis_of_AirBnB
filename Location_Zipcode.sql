73e111f3712eef547a8828b49c544557ca3fb955
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-11 17:47:29 +0530

    Update Location_Zipcode.sql

--- Location_Zipcode.sql
+++ Location_Zipcode.sql
@@ -1,2 +1,30 @@
 CREATE TABLE Location_Zipcode(Zip_id INTEGER PRIMARY KEY,
- 						   Zipcode VARCHAR(50));
+ 						                       Zipcode VARCHAR(50));
+ ---------------------YUVRAJ---------------------------                             
+ INSERT INTO Location_Zipcode VALUES(1,'V6E 2P4');
+ INSERT INTO Location_Zipcode VALUES(2,'V5Y');
+ INSERT INTO Location_Zipcode VALUES(3,'V5Y');
+ INSERT INTO Location_Zipcode VALUES(4,'V6B');
+ INSERT INTO Location_Zipcode VALUES(5,'V6Z 2S1');
+ INSERT INTO Location_Zipcode VALUES(6,'V5N');
+ INSERT INTO Location_Zipcode VALUES(7,'V5V1Y8');
+ INSERT INTO Location_Zipcode VALUES(8,'V6Z');
+ INSERT INTO Location_Zipcode VALUES(9,'V5K');
+ INSERT INTO Location_Zipcode VALUES(10,'V5N 1T6');
+ INSERT INTO Location_Zipcode VALUES(11,'V5M 2V4');
+ INSERT INTO Location_Zipcode VALUES(12,'V5Y');
+ INSERT INTO Location_Zipcode VALUES(13,'V5T');
+ INSERT INTO Location_Zipcode VALUES(14,'V5L 2N1');
+ INSERT INTO Location_Zipcode VALUES(15,'V6E');
+ INSERT INTO Location_Zipcode VALUES(16,'V6H 1N7');
+ INSERT INTO Location_Zipcode VALUES(17,'V5T 2V6');
+ INSERT INTO Location_Zipcode VALUES(18,'V6Z 2S1');
+ INSERT INTO Location_Zipcode VALUES(19,'V5R 2V8');
+ INSERT INTO Location_Zipcode VALUES(20,'V5R 2V8');
+ INSERT INTO Location_Zipcode VALUES(21,'V6K 2M8');
+ INSERT INTO Location_Zipcode VALUES(22,'V6Z');
+ INSERT INTO Location_Zipcode VALUES(23,'V6B');
+ INSERT INTO Location_Zipcode VALUES(24,'V5L 2H3');
+ INSERT INTO Location_Zipcode VALUES(25,'V6Z 1P6');
+ ----------------Sartaaj----------------------
+ ----------------Aditi--------------------------
