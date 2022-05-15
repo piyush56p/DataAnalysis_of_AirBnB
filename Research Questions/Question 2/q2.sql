ee0c8137f15bb5cfbe697b38173de9c7e54a3784
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-15 16:28:10 +0530

    Update q2.sql

--- Research Questions/Question 2/q2.sql
+++ Research Questions/Question 2/q2.sql
@@ -1,1 +1,18 @@
+--Q2 Does the 'review score' vary depending on the 'room type'?
 
+SELECT 
+	P.Room_type,
+	AVG(Review_score_rating) as AVG_Score,
+	MAX(RR.Review_score_rating) as MAX_Score,
+	MIN(RR.Review_score_rating) as MIN_Score,
+	COUNT(RR.Review_score_rating) as Total
+FROM
+	Review_Rating RR
+	JOIN
+		reviews R
+		ON R.Review_id = RR.Review_id
+	JOIN 
+		Property P
+		ON P.Review_id = R.Review_id
+	GROUP BY
+		P.Room_type;
