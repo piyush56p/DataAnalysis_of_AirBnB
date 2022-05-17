d63f070a0112e9c9f1bf5dd97571eb0c62f8ff8a
Piyush Sharma <70280797+piyush56p@users.noreply.github.com>
2022-05-17 15:21:01 +0530

    Update q1.sql

--- PLSQL/q1.sql
+++ PLSQL/q1.sql
@@ -4,14 +4,27 @@ DECLARE
      FROM Property;
      ACCO Property.Accommodations%TYPE;
      --PRICE Property.Price%TYPE;
+     CURSOR B IS SELECT Price
+     FROM Property;
+     PRICE Property.Price%TYPE;
      
      C NUMBER;
 BEGIN
     OPEN S;
+    OPEN B;
     LOOP
         FETCH S INTO ACCO;
         exit WHEN S%NOTFOUND;
         dbms_output.put_line(ACCO);
+    
+    --CLOSE S;
     END LOOP;
-    CLOSE S;
+    LOOP
+        FETCH B INTO PRICE;
+        exit WHEN B%NOTFOUND;
+        dbms_output.put_line(PRICE);
+    END LOOP;
+    --CLOSE B;
+    
+    
 END;
